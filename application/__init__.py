251afc600db8850865c8bb3ca51d58745891c817
Joel Hassan <joel.nhn@gmail.com>
2020-05-15 01:31:31 +0300

    progress

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+
+# configure database URI
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///services.db"
+
+# log all queries
+app.config["SQLALCHEMY_ECHO"] = True
+
+# db object for interacting with the db
+db = SQLAlchemy(app)
+
 from application import views
+
+from application.services import models
+
+# creates all the db relations
+db.create_all()

fadd95acf6a60e7f1f46131e6290d3217b5b0866
Joel Hassan <joel.nhn@gmail.com>
2020-05-17 18:30:04 +0300

    forms

--- application/__init__.py
+++ application/__init__.py
@@ -5,22 +5,24 @@ app = Flask(__name__)
 
 
 # Env config - see SQLAlchemy docs
-ENV = 'prod'
+ENV = "dev"
 
-if ENV == 'dev':
+if ENV == "dev":
     app.debug = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:123456@localhost/devServices"
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgresql://postgres:123456@localhost/devServices"
 else:
     app.debug = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://lzhvtfufywmlwi:e28454e9e7ca660b7705a31b201b1dadaddfffa04045034cc353450388d14f69@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d1jvsaumt4o3pl"
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgres://lzhvtfufywmlwi:e28454e9e7ca660b7705a31b201b1dadaddfffa04045034cc353450388d14f69@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d1jvsaumt4o3pl"
 
-# # configure database URI
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///services.db"
 
 # log all queries
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SECRET_KEY'] = '6391c84926554768c34560ab1ff4d839'
+app.config["SECRET_KEY"] = "6391c84926554768c34560ab1ff4d839"
 
 # db object for interacting with the db - can use it to query db
 db = SQLAlchemy(app)
--- application/routes.py
+++ application/routes.py
@@ -1,28 +1,57 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
+
 # app needs to be imported as it's used by decorators below
 from application import app
+from application.forms import RegistrationForm, LoginForm
+
+
+services = [
+    "Mobile",
+    "Web",
+    "Design",
+    "DevOps",
+]
+
+developers = [
+    {"name": "John Doe", "skills": ["html, css, JavaScript"],},
+    {"name": "Jane Doe", "skills": ["Docker, Node, CI"],},
+]
+
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("index.html", services=services, developers=developers)
 
-@app.route("/register")
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@agency.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+    return render_template("login.html", title="Login", form=form)
 
-@app.route("/submit", methods=['POST'])
+
+@app.route("/submit", methods=["POST"])
 def submit():
-    if request.method == 'POST':
-      client_name = request.form['client-name']
-      client_phone_number = request.form['client-phone-number']
-      """ TODO: rest of form data """
-      print(client_name)
-      print(client_phone_number)
-      if client_name == '' or client_phone_number == '':
-        return render_template('index.html', message='Please enter required fields')
+    if request.method == "POST":
+        client_name = request.form["client-name"]
+        client_phone_number = request.form["client-phone-number"]
+        """ TODO: rest of form data """
+        print(client_name)
+        print(client_phone_number)
+        if client_name == "" or client_phone_number == "":
+            return render_template("index.html", message="Please enter required fields")
     return render_template("success.html")
-

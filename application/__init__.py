e9e9b7ed749572f10e179b281b8f8178f8c72966
Joel Hassan <joel.nhn@gmail.com>
2020-05-18 01:14:58 +0300

    added database entities

--- application/__init__.py
+++ application/__init__.py
@@ -20,12 +20,57 @@ else:
 
 
 # log all queries
-app.config["SQLALCHEMY_ECHO"] = True
+# app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "6391c84926554768c34560ab1ff4d839"
 
 # db object for interacting with the db - can use it to query db
 db = SQLAlchemy(app)
+
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp(),
+    )
+    """ Role: will get all roles for this account - referencing the 'Role' class
+        backref: the account can be referenced from Role by the backref 
+        lazy=True - db will load data in one go as necessary
+     """
+    role = db.relationship("Role", backref="account", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.password}')"
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), unique=True, nullable=False)
+    account_id = db.Column(db.Integer, db.ForeignKey("account.id"), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}')"
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp(),
+    )
+
+    def __repr__(self):
+        return f"User('{self.date_created}', '{self.date_modified}')"
+
 
 from application import routes
 

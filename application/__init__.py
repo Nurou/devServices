8aaaa956503730ad1e8fa0f84334792d96ab2854
Joel Hassan <joel.nhn@gmail.com>
2020-05-25 22:28:45 +0300

    trying again

--- application/__init__.py
+++ application/__init__.py
@@ -20,7 +20,7 @@ else:
     ] = "postgres://lzhvtfufywmlwi:e28454e9e7ca660b7705a31b201b1dadaddfffa04045034cc353450388d14f69@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d1jvsaumt4o3pl"
 
 # log all queries
-# app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "6391c84926554768c34560ab1ff4d839"
 

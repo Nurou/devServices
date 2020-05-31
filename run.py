81ce74066994e76373e0771504fe9b6e8e958df3
Joel Hassan <joel.nhn@gmail.com>
2020-05-31 17:56:12 +0300

    add procfile

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-# single responsibility: run app
-# imports from other packages happen through the __init__.py file
 from application import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run()
-

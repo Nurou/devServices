44e49cef72e11cc1276e8c74df21f506b1224177
Joel Hassan <joel.nhn@gmail.com>
2020-06-14 17:09:10 +0300

    small UI changes

--- application/main/routes.py
+++ application/main/routes.py
@@ -7,5 +7,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def home():
     if current_user.is_authenticated:
+        if current_user.role.name == 'ADMIN':
+          return redirect(url_for("accounts.dashboard"))
         return redirect(url_for("accounts.account"))
     return render_template("index.html")
